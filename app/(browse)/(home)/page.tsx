80a9cc0ccf3ebf9b764cf8e5af383bde4f099625
sdfwefk11 <mknhsad3398@naver.com>
2024-02-10 21:06:23 +0900

    test

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -1,18 +1,3 @@
-import { Suspense } from "react";
-import { Movies, MoviesSkeleton } from "./_components/movies";
-import { Separator } from "@/components/ui/separator";
-import { Footer } from "./_components/footer";
-
 export default function Home() {
-  return (
-    <Suspense fallback={<MoviesSkeleton />}>
-      <div>
-        <div className="flex flex-col items-center justify-center pt-12 pb-20">
-          <Movies />
-        </div>
-        <Separator />
-        <Footer />
-      </div>
-    </Suspense>
-  );
+  return <div>dfdfdf</div>;
 }
