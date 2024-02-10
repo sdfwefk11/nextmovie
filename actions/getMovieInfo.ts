b17e4a5aeb5dedf47dde8bcd552d4e4a90f1b9a5
sdfwefk11 <mknhsad3398@naver.com>
2024-02-10 18:18:03 +0900

    build

--- actions/getMovieInfo.ts
+++ actions/getMovieInfo.ts
@@ -3,7 +3,7 @@ import { fetchMovieDetail } from "@/fetchs/fetch-movie-detail";
 
 export async function getMovieInfo(movieId: string) {
   try {
-    await new Promise((resolve) => setTimeout(resolve, 10000));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     const movieDetail = await fetchMovieDetail(movieId);
     return movieDetail;
   } catch {
