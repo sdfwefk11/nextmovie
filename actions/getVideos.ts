e94d0246e8e7fa9dcc4deb7dc22a0f1170be4cff
sdfwefk11 <mknhsad3398@naver.com>
2024-02-10 22:14:33 +0900

    test

--- actions/getVideos.ts
+++ actions/getVideos.ts
@@ -2,7 +2,7 @@
 import { fetchYoutubeIframe } from "@/fetchs/fetch-youtube-iframe";
 import { revalidatePath } from "next/cache";
 
-export async function getVideos(movieId: string) {
+export async function getVideos(movieId: number) {
   try {
     await new Promise((resolve) => setTimeout(resolve, 1000));
     const videos = await fetchYoutubeIframe(movieId);
