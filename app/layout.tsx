549cdb6d43fa4b69403d559bb498204f2c38c571
sdfwefk11 <mknhsad3398@naver.com>
2024-02-22 03:08:36 +0900

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark">
           {children}
-          {/* <Analytics /> */}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
